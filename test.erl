bbd4e10bd7d44d94bfd467c5b0c64f36a352653a
Kristian Ionescu <ionescu.kristian@gmail.com>
2015-12-10 23:23:37 +0100

    changes to test.erl
    Added histogram sum and distribution function and normalize.

--- test.erl
+++ test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([mergeSort/1, occurance/2, histo/2, histogram/1]).
+-export([mergeSort/1, occurance/2, histo/2, histogram/1, histosum/1, histodist/1, normalize/1]).
 
 mergeSort([]) -> [];
 mergeSort([Y]) -> [Y];
@@ -14,31 +14,21 @@ occurance(N, [H|T]) ->
   end.
 
 % [H|T] has to be a sorted list %
+% Builds a proplist with keys from N to 20 where the value associated
+% with each key is based on the # of occurances in list L.
 histo(_,N) when N > 20 -> [];
 histo([H|T], N) when H == N -> [{N, occurance(H, [H|T])}] ++ histo([X || X <- T, X /= H], N+1);
 histo(L, N) -> [{N, 0}] ++ histo(L, N+1).
 
 histogram(L) -> histo(mergeSort(L), 1).
 
+% Takes a list of proplists and sums the values for matching keys
+histosum([]) -> [];
+histosum([H|T]) -> orddict:merge(fun(_K, V1,V2) -> V1+V2 end, H, histosum(T)).
 
-%orddict:merge(fun(_Key, V1,V2)-> V1+V2 end, X, Y).
-%[{1,2},
-%{2,4},
-%{3,0},
-%{4,2},
-%{5,10},
-%{6,0},
-%{7,0},
-%{8,0},
-%{9,0},
-%{10,0},
-%{11,0},
-%{12,0},
-%{13,0},
-%{14,0},
-%{15,0},
-%{16,0},
-%{17,0},
-%{18,0},
-%{19,0},
-%{20,0}]
+% Takes a proplist as parameter, For each value in proplist, divides the value by 100 and appends to empty list.
+histodist(L) -> [V/100 || {_K,V} <- L , V /= 0].
+
+normalize(L) ->
+  LSUM = histosum(L),
+  {LSUM, histodist(LSUM)}.
